0ad0b92ccb84f21f2ddc52fff92543b117561b35
wallercullen <wallercl@dukes.jmu.edu>
2023-05-04 13:29:02 -0400

    playlist search

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,7 @@
 const weatherAPIKey = "afa769abcb584e5080a195837230305";
+const clientId = 'ba3168bc03e94f21b9fa1e2678bca0a4';
+const redirectUri = 'https://wallercullen.github.io/343-s23-p3/';
+
 
 const getWeather = (word) => {
     console.log("attempting to get weather for", word);
@@ -7,6 +10,18 @@ const getWeather = (word) => {
     ).then((resp) => resp.json());
 };
 
+async function searchPlaylist(query) {
+    let accessToken = localStorage.getItem('access_token');
+
+    const response = await fetch(`https://api.spotify.com/v1/search?q=${query}&type=playlist`, {
+        headers: {
+            Authorization: 'Bearer ' + accessToken
+        }
+    });
+
+    const data = await response.json();
+    return data;
+}
 
 function generateRandomString(length) {
     let text = '';
@@ -33,42 +48,26 @@ async function generateCodeChallenge(codeVerifier) {
     return base64encode(digest);
 }
 
-const clientId = 'ba3168bc03e94f21b9fa1e2678bca0a4';
-const redirectUri = 'https://wallercullen.github.io/343-s23-p3/';
-
 function LogInSpotify() {
     let codeVerifier = generateRandomString(128);
 
     generateCodeChallenge(codeVerifier).then(codeChallenge => {
-    let state = generateRandomString(16);
-    let scope = 'user-read-private user-read-email';
+        let state = generateRandomString(16);
+        let scope = 'user-read-private user-read-email';
 
-    localStorage.setItem('code_verifier', codeVerifier);
+        localStorage.setItem('code_verifier', codeVerifier);
 
-    let args = new URLSearchParams({
-        response_type: 'code',
-        client_id: clientId,
-        scope: scope,
-        redirect_uri: redirectUri,
-        state: state,
-        code_challenge_method: 'S256',
-        code_challenge: codeChallenge
-    });
+        let args = new URLSearchParams({
+            response_type: 'code',
+            client_id: clientId,
+            scope: scope,
+            redirect_uri: redirectUri,
+            state: state,
+            code_challenge_method: 'S256',
+            code_challenge: codeChallenge
+        });
 
-    window.location = 'https://accounts.spotify.com/authorize?' + args;
-    });
-
-    const urlParams = new URLSearchParams(window.location.search);
-    let code = urlParams.get('code');
-
-    codeVerifier = localStorage.getItem('code_verifier');
-
-    let body = new URLSearchParams({
-    grant_type: 'authorization_code',
-    code: code,
-    redirect_uri: redirectUri,
-    client_id: clientId,
-    code_verifier: codeVerifier
+        window.location = 'https://accounts.spotify.com/authorize?' + args;
     });
 }
 
@@ -93,5 +92,43 @@ searchForm.onsubmit = (ev) => {
         is_day = weatherResults['current']['is_day'];
         console.log(weather);
         console.log(is_day ? "day" : "night")
+    });
+
+
+    const urlParams = new URLSearchParams(window.location.search);
+    let code = urlParams.get('code');
+    let codeVerifier = localStorage.getItem('code_verifier');
+
+    let body = new URLSearchParams({
+        grant_type: 'authorization_code',
+        code: code,
+        redirect_uri: redirectUri,
+        client_id: clientId,
+        code_verifier: codeVerifier
+    });
+
+    const response = fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: body
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('HTTP status ' + response.status);
+        }
+        return response.json();
+    })
+    .then(data => {
+        localStorage.setItem('access_token', data.access_token);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
+    let query = weather + "%20" + is_day ? "day" : "night";
+    searchPlaylist(query).then(data => {
+        console.log(data);
     });
 };
